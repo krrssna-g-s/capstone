1c276a39f6864cc287d874f5bc817e4d5bdc5039
Srikrishna Gumma <srikrishna.gumma@gmail.com>
2026-04-01 11:35:05 -0400

    initial commit

--- src/App.js
+++ src/App.js
@@ -2,9 +2,19 @@ import './App.css';
 
 function App() {
   return (
-    <div className="App">
+    <>
+    <header>
+      <h1>Welcome to my React App</h1>
+    </header>
+    <nav>
+      <ul>
+        <li><a href="/">Home</a></li>
+        <li><a href="/about">About</a></li>
+        <li><a href="/contact">Contact</a></li>
+      </ul>
+    </nav>
       Home page
-    </div>
+    </>
   );
 }
 
